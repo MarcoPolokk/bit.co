4bd0898ed241b01da6797b2b811b1959e622e51b
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2020-01-21 16:57:48 +0100

    Initial Commit

---
+++ bit.co/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bit.co
+//
+//  Created by Paweł Kozioł on 21/01/2020.
+//  Copyright © 2020 Paweł Kozioł. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
